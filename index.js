9b9e65419daf497723501180ff2efa9c43b925fe
jimmy0006 <jinmin645@gmail.com>
2023-03-06 23:59:38 +0900

    cors

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const app = express();
 const admin = require('./router/admin')
 const result = require('./router/result')
 const cookieParser = require('cookie-parser')
+const cors = require('cors')
 require('dotenv').config()
 
 app.use(cookieParser(process.env.SECRET))
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(cors())
 
 app.use('/admin',admin);
 app.use('*',(req,res,next)=>{
